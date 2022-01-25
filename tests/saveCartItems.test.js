5ece9c99d661fd127a62b90dba3060d4f8bf4975
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-24 22:23:14 -0400

    Requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Test when execute saveCartItems with param "<ol><li>Item</li></ol>", localStorage.setItem must be called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Test when execute saveCartItems with param "<ol><li>Item</li></ol>", localStorage.setItem must be called with correct parameters', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'cartItems',
+      '<ol><li>Item</li></ol>',
+    );
+  });
 });
