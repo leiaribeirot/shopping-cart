65396eece90096634fdd42470d5bd5e3259a8d04
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-24 21:10:52 -0400

    Requisito 1 e 8

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const fetchProducts = async (mouse) => {
+    const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${mouse}`;
+    
+    const result = await fetch(API_URL)
+     .then((response) => response.json())
+     .then((data) => data)
+
+    .catch((error) => error); 
+
+    return result;
+  };
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Test se fetchProducts é uma função', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Test se o parametro passado é o corrreto.', async () => {
+    expect.assertions(1);// expect 1 assertion
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Test se o endpoint esta correto', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador");
+  });
+  //using mock docs to compare
+  it('Test se o objeto retornado esta correto', async () => {
+    expect.assertions(1);
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+  it('Test se nenhum parametro passado retorna um erro', async () => {
+    try {
+      await fetchProducts();
+    } catch (error) {
+      expect(error).toEqual(new Error('É necessario uma URL'));
+    };    
+});
 });
